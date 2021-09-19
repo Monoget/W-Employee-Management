6a38b1fcfa06dc66f1b816ec1180805d5380c38d
Monoget <monoget2@gmail.com>
2021-09-19 16:34:11 +0600

    update Files

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,6 +28,9 @@ class LoginController extends Controller
      */
     protected $redirectTo = RouteServiceProvider::HOME;
 
+    protected $maxAttempts=3;
+    protected $decayMinutes=5;
+
     /**
      * Create a new controller instance.
      *
